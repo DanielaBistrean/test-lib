f9e0c3993b2d31b1530bc1dc3a0e5ef595f10b9b
Daniela Marinela Bistrean <daniela.marinela.bistrean@gmail.com>
2020-12-29 18:13:42 +0200

    Implement the library

---
+++ foo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void foo (void)
+{
+    puts("This is a shared library");
+}         
+          
+          
---
+++ foo.h
@@ -0,0 +1,5 @@
+#ifndef _foo_h_
+#define _foo_h_
+
+extern void foo (void);
+#endif
